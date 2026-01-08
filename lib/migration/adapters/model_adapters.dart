df106937ca4258fba44b566f7b6965bb76620a4b
R <racheli.grayover@gmail.com>
2026-01-08 15:17:23 +0200

    מחיקה של קבצים לא בשימוש

--- lib/migration/adapters/model_adapters.dart
+++ lib/migration/adapters/model_adapters.dart
@@ -1,37 +1,10 @@
-// Adapters to convert between otzaria models and migration models
+﻿// Adapters to convert between otzaria models and migration models
 // This allows seamless integration between the existing codebase and the new database layer
 
 import 'package:otzaria/models/books.dart' as otzaria_models;
-import 'package:otzaria/library/models/library.dart' as otzaria_lib;
-import 'package:otzaria/migration/core/models/book.dart' as migration_models;
-import 'package:otzaria/migration/core/models/category.dart'
-    as migration_models;
 import 'package:otzaria/migration/core/models/line.dart' as migration_models;
 import 'package:otzaria/migration/core/models/toc_entry.dart'
     as migration_models;
-
-/// Converts a migration Book to an otzaria TextBook
-otzaria_models.TextBook migrationBookToOtzariaBook(
-  migration_models.Book migrationBook,
-  otzaria_lib.Category? category,
-) {
-  return otzaria_models.TextBook(
-    title: migrationBook.title,
-    category: category,
-    author: migrationBook.authors.isNotEmpty
-        ? migrationBook.authors.first.name
-        : null,
-    heShortDesc: migrationBook.heShortDesc,
-    pubDate: migrationBook.pubDates.isNotEmpty
-        ? migrationBook.pubDates.first.date
-        : null,
-    pubPlace: migrationBook.pubPlaces.isNotEmpty
-        ? migrationBook.pubPlaces.first.name
-        : null,
-    order: migrationBook.order.toInt(),
-    topics: migrationBook.topics.map((t) => t.name).join(', '),
-  );
-}
 
 /// Converts a migration TocEntry to an otzaria TocEntry
 otzaria_models.TocEntry migrationTocToOtzariaToc(
@@ -51,20 +24,4 @@ otzaria_models.TocEntry migrationTocToOtzariaToc(
 /// Converts a list of migration Lines to a single text string
 String migrationLinesToText(List<migration_models.Line> lines) {
   return lines.map((line) => line.content).join('\n');
-}
-
-/// Converts a migration Category to an otzaria Category
-otzaria_lib.Category migrationCategoryToOtzariaCategory(
-  migration_models.Category migrationCategory,
-  otzaria_lib.Category? parent,
-) {
-  return otzaria_lib.Category(
-    title: migrationCategory.title,
-    description: '',
-    shortDescription: '',
-    order: 999,
-    subCategories: [],
-    books: [],
-    parent: parent,
-  );
 }
