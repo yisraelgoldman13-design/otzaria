4a00967944a09a3fb5f7d0b291756031ff47b66a
Y-PLONI <7353755@gmail.com>
2026-01-08 23:44:21 +0200

    מחיקת קבצים לא רלוונטיים

--- update_version.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running version update script...
-powershell -ExecutionPolicy Bypass -File update_version.ps1
-if %ERRORLEVEL% EQU 0 (
-    echo Version update completed successfully!
-) else (
-    echo Version update failed!
-)
-pause
